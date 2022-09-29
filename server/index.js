418f760beb3eca27c31a5e021fdf50a0caffd21d
wesleyangeli <wesleyangeli@gmail.com>
2022-09-29 20:33:16 -0300

    create server

---
+++ server/index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const UserModel = require("./models/Users");
+const url = "mongodb://127.0.0.1:27017/aluguelMaquinas";
+const cors = require("cors");
+
+app.use(express.json());
+app.use(cors());
+
+mongoose.connect(url);
+
+app.get("/getUsers", (req, res) => {
+  UserModel.find({}, (err, result) => {
+    if (err) {
+      res.json(err);
+    } else {
+      res.json(result);
+    }
+  });
+});
+
+app.post("/createUser", async (req, res) => {
+  const user = req.body;
+  const newUser = new UserModel(user);
+  await newUser.save();
+
+  res.json(user);
+});
+
+app.listen(3001, () => {
+  console.log("SERVER RUNS PERFECTLY");
+});
